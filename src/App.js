6187f5c85fb974372199a86c1e1b43e657a51f1c
JRCR-GMAIL-NEO2K16 <jrcr.carrion@gmail.com>
2022-11-21 18:57:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1
+          style={{
+            color: "red",
+          }}
+        >
+          CURSO DE UDEMY SOBRE DOCKER Y KUBERNETES !!
+        </h1>
+        <h2
+          style={{
+            color: "yellow",
+            backgroundColorj: "red",
+          }}
+        >
+          MI MAMI SE VA A PONER MEJOR.....!!!!
+        </h2>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
